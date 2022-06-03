37815ec3fb28f38d2e65209417c5ca4ab3ef408d
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-06-04 01:01:03 +0300

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import "./pages.css";
+
+
+const Home = () => {
+ 
+
+  
+
+ 
+
+  return (
+    <>
+      <div className="content">
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
